3d6c60e8e4007392f878ce00896349ba3dbd9ca5
Briac Le Guillou <briacleguilloux@gmail.com>
2025-07-09 13:43:02 +0200

    add models for notebook

--- src/fct.py
+++ src/fct.py
@@ -1,4 +1,5 @@
 import numpy as np
+import torch
 import torch.nn as nn
 
 def move_sliding_window(data, window_size, inputs_cols_indices, label_col_index):
@@ -23,5 +24,57 @@ def move_sliding_window(data, window_size, inputs_cols_indices, label_col_index)
 
 
 
-    
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+class GRUNet(nn.Module):
+    def __init__(self, input_dim, hidden_dim, output_dim, n_layers, drop_prob=0.2):
+        super(GRUNet, self).__init__()
+        self.hidden_dim = hidden_dim
+        self.n_layers = n_layers
+
+        self.gru = nn.GRU(
+            input_dim, hidden_dim, n_layers, batch_first=True, dropout=drop_prob
+        )
+        self.fc = nn.Linear(hidden_dim, output_dim)
+        self.relu = nn.ReLU()
+
+    def forward(self, x, h):
+        out, h = self.gru(x, h)
+        out = self.fc(self.relu(out[:, -1]))
+        return out, h
+
+    def init_hidden(self, batch_size):
+        weight = next(self.parameters()).data
+        hidden = (
+            weight.new(self.n_layers, batch_size, self.hidden_dim).zero_().to(device)
+        )
+        return hidden
+
+
+class LSTMNet(nn.Module):
+    def __init__(self, input_dim, hidden_dim, output_dim, n_layers, drop_prob=0.2):
+        super(LSTMNet, self).__init__()
+        self.hidden_dim = hidden_dim
+        self.n_layers = n_layers
+
+        self.lstm = nn.LSTM(
+            input_dim, hidden_dim, n_layers, batch_first=True, dropout=drop_prob
+        )
+        self.fc = nn.Linear(hidden_dim, output_dim)
+        self.relu = nn.ReLU()
+
+    def forward(self, x, h):
+        out, h = self.lstm(x, h)
+        out = self.fc(self.relu(out[:, -1]))
+        return out, h
+
+    def init_hidden(self, batch_size):
+        weight = next(self.parameters()).data
+        hidden = (
+            weight.new(self.n_layers, batch_size, self.hidden_dim)
+            .zero_()
+            .to(device),
+            weight.new(self.n_layers, batch_size, self.hidden_dim).zero_().to(device),
+        )
+        return hidden
 
